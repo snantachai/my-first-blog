f7f4d70a3ecb2f94aca2907adba320bfa7970895
Joe Sir <snantachai@gmail.com>
2019-12-05 00:09:16 +0700

    add draft list and publish button

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
 	published_date = models.DateTimeField(blank=True, null=True)
 
 
-	def pusblish(self):
+	def publish(self):
 		self.published_date = timezone.now()
 		self.save
 
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,10 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
 
+def post_draft_list(request):
+	posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+	return render(request, 'blog/post_draft_list.html', {'posts':posts})
+
 def post_detail(request, pk):
 	post = Post.objects.get(pk=pk)
 	return render(request, 'blog/post_detail.html', {'post':post})
@@ -18,7 +22,6 @@ def post_new(request):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:	
@@ -32,9 +35,15 @@ def post_edit(request, pk):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})	
+
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=pk)
+
+	
